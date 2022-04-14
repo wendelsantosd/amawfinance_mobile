9a65785419b95bc479d8baac4c23f05639bb6161
WendelSantosD <wendelwcsantos@gmail.com>
2022-04-14 20:49:09 -0300

    feat: implements layout login page

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -12,6 +12,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool showPassword = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +23,8 @@ class _LoginState extends State<Login> {
         child: ListView(
           children: [
             SizedBox(
-              width: 200,
-              height: 200,
+              width: 150,
+              height: 150,
               child: Image.asset(AppImages.logo),
             ),
             Center(
@@ -30,7 +32,100 @@ class _LoginState extends State<Login> {
                 'Amaw Finance',
                 style: TextStyles.title,
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+                labelStyle: TextStyles.primaryStyleFont,
+              ),
+              style: TextStyles.primaryStyleFont,
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: showPassword ? false : true,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                labelStyle: TextStyles.primaryStyleFont,
+                suffixIcon: GestureDetector(
+                    child: Icon(
+                      showPassword ? Icons.visibility_off : Icons.visibility,
+                      color: AppColors.grey300,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    }),
+              ),
+              style: TextStyles.primaryStyleFont,
+            ),
+            Container(
+              height: 50,
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () => print('Navigate'),
+                child: Text(
+                  'Recuperar Senha',
+                  textAlign: TextAlign.right,
+                  style: TextStyles.smallFont,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 50,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: AppColors.blue500,
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              child: SizedBox.expand(
+                child: TextButton(
+                  onPressed: () => print('Login'),
+                  child: Text(
+                    'ENTRAR',
+                    style: TextStyles.fontInnerPrimaryButton,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 50,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: AppColors.white400,
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              child: SizedBox.expand(
+                child: TextButton.icon(
+                  icon: Image.network(
+                    'http://pngimg.com/uploads/google/google_PNG19635.png',
+                    fit: BoxFit.cover,
+                  ),
+                  onPressed: () => print('Login'),
+                  label: Text(
+                    'ENTRAR COM GOOGLE',
+                    style: TextStyles.fontInnerGoogleButton,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () => print('Navigate'),
+                child: Text(
+                  'Criar Conta',
+                  textAlign: TextAlign.center,
+                  style: TextStyles.smallFont,
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/shared/themes/app_text_styles.dart
+++ lib/shared/themes/app_text_styles.dart
@@ -9,4 +9,30 @@ class TextStyles {
     fontWeight: FontWeight.w600,
     color: AppColors.black500,
   );
+
+  static final primaryStyleFont = GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.grey300,
+  );
+
+  static final smallFont = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.red600,
+  );
+
+  static final fontInnerPrimaryButton = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    letterSpacing: 2,
+  );
+
+  static final fontInnerGoogleButton = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black600,
+    letterSpacing: 2,
+  );
 }
