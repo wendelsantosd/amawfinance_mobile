c33742f4873eebbeb31193ac316213939a2abfc8
WendelSantosD <wendelwcsantos@gmail.com>
2022-04-13 23:35:35 -0300

    feat: add logo in login page.

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/themes/app_colors.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -10,8 +12,19 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Tela de login'),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 60, right: 40, left: 40),
+        color: AppColors.background,
+        child: ListView(
+          children: [
+            SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/logo.png')),
+          ],
+        ),
+      ),
     );
   }
 }
