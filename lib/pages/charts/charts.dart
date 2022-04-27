cff952427b895a782814b2137ad2574dfea6e88d
WendelSantosD <wendelwcsantos@gmail.com>
2022-04-27 00:29:54 -0300

    feat: print value in chart

--- lib/pages/charts/charts.dart
+++ lib/pages/charts/charts.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:amawfinance_mobile/components/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
@@ -11,49 +10,63 @@ class Charts extends StatefulWidget {
   _ChartsState createState() => _ChartsState();
 }
 
-class Sales {
-  String year;
-  int sales;
+class Sum {
+  String month;
+  int amount;
 
-  Sales(this.year, this.sales);
+  Sum(this.month, this.amount);
 }
 
 class _ChartsState extends State<Charts> {
-  List<Sales> _data = [];
-  List<charts.Series<Sales, String>> _chartdata = [];
+  List<Sum> _data = [];
+  List<charts.Series<Sum, String>> _chartData = [];
+  dynamic value = 'Nenhum mês selecionado';
+  dynamic month = '';
+
+  setValue(state) {
+    setState(() {
+      value = state;
+    });
+  }
+
+  setMonth(state) {
+    setState(() {
+      month = state;
+    });
+  }
 
   void _makeData() {
-    _data = <Sales>[];
-    _chartdata = <charts.Series<Sales, String>>[];
+    _data = <Sum>[];
+    _chartData = <charts.Series<Sum, String>>[];
 
     final rnd = Random();
 
     // for (int i = 2010; i < 2019; i++) {
-    //   _data.add(Sales(i.toString(), rnd.nextInt(1000)));
+    //   _data.add(Sum(i.toString(), rnd.nextInt(1000)));
     // }
 
-    _data.add(Sales('Jan', rnd.nextInt(1000)));
-    _data.add(Sales('Fev', rnd.nextInt(1000)));
-    _data.add(Sales('Mar', rnd.nextInt(1000)));
-    _data.add(Sales('Abr', rnd.nextInt(1000)));
-    _data.add(Sales('Mai', rnd.nextInt(1000)));
-    _data.add(Sales('Jun', rnd.nextInt(1000)));
-    _data.add(Sales('Jul', rnd.nextInt(1000)));
-    _data.add(Sales('Ago', rnd.nextInt(1000)));
-    _data.add(Sales('Set', rnd.nextInt(1000)));
-    _data.add(Sales('Out', rnd.nextInt(1000)));
-    _data.add(Sales('Nov', rnd.nextInt(1000)));
-    _data.add(Sales('Dez', rnd.nextInt(1000)));
+    _data.add(Sum('Jan', rnd.nextInt(1000)));
+    _data.add(Sum('Fev', rnd.nextInt(1000)));
+    _data.add(Sum('Mar', rnd.nextInt(1000)));
+    _data.add(Sum('Abr', rnd.nextInt(1000)));
+    _data.add(Sum('Mai', rnd.nextInt(1000)));
+    _data.add(Sum('Jun', rnd.nextInt(1000)));
+    _data.add(Sum('Jul', rnd.nextInt(1000)));
+    _data.add(Sum('Ago', rnd.nextInt(1000)));
+    _data.add(Sum('Set', rnd.nextInt(1000)));
+    _data.add(Sum('Out', rnd.nextInt(1000)));
+    _data.add(Sum('Nov', rnd.nextInt(1000)));
+    _data.add(Sum('Dez', rnd.nextInt(1000)));
 
-    _chartdata.add(
+    _chartData.add(
       charts.Series(
-        id: 'Sales',
+        id: 'Sum',
         colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
         data: _data,
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
+        domainFn: (Sum amount, _) => amount.month,
+        measureFn: (Sum amount, _) => amount.amount,
         fillPatternFn: (_, __) => charts.FillPatternType.solid,
-        displayName: 'Sales',
+        displayName: 'Sum',
       ),
     );
   }
@@ -73,9 +86,26 @@ class _ChartsState extends State<Charts> {
         padding: const EdgeInsets.all(20),
         child: Column(children: [
           const SizedBox(height: 100),
-          const Text('Sales Data'),
+          Text('$month R\$ $value'),
           Expanded(
-            child: charts.BarChart(_chartdata),
+            child: charts.BarChart(
+              _chartData,
+              selectionModels: [
+                charts.SelectionModelConfig(
+                  changedListener: (charts.SelectionModel model) {
+                    setValue(
+                      model.selectedSeries[0]
+                          .measureFn(model.selectedDatum[0].index),
+                    );
+
+                    setMonth(
+                      model.selectedSeries[0]
+                          .domainFn(model.selectedDatum[0].index),
+                    );
+                  },
+                )
+              ],
+            ),
           )
         ]),
       ),
